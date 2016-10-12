65917154f97e7321117ce1b4ac1b033755facefd
davidfelcey <davefelcey@gmail.com>
2016-10-12 18:39:07 +0100

    Corrected queries

--- geoQuery.groovy
+++ geoQuery.groovy
@@ -1,6 +1,5 @@
-// Get sensors with in range of a point
-g.V().has("sensor", "loc", Geo.inside(Geo.distance(50, 25, 15)))
-// Get sensors that cover a certain area 
-g.V().has("sensor", "loc", Geo.inside(Geo.distance(50, 25, 15)))
-
+// Get sensors with in range of a point - Geo.distance is (x,y,radius)
+g.V().hasLabel('sensor').out('hasPoint').has('loc', Geo.inside(Geo.distance(10,30,50)))
+// Get sensors that cover a certain area inside another area 
+g.V().hasLabel('sensor').out('hasRegion').has('reg', Geo.inside(Geo.polygon(0.0,0.0,0.0,100.0,100.0,100.0,100.0,0.0)))
 
